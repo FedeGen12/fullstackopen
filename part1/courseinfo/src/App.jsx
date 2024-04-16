022f417022ebfa35cedfd053a07e0553180923b5
Federico Genaro <fedegen14@gmail.com>
2024-04-16 07:32:44 -0300

    Ejercicio 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,7 +6,7 @@ const Header = (props) => {
     )
 }
 
-const Content = (props) => {
+const Part = (props) => {
     return (
         <p>
             {props.part} {props.exercise}
@@ -36,9 +36,9 @@ const App = () => {
         <div>
             <Header course={course} />
 
-            <Content part={part1} exercise={exercises1} />
-            <Content part={part2} exercise={exercises2} />
-            <Content part={part3} exercise={exercises3} />
+            <Part part={part1} exercise={exercises1} />
+            <Part part={part2} exercise={exercises2} />
+            <Part part={part3} exercise={exercises3} />
 
             <Total total={exercises1 + exercises2 + exercises3} />
         </div>
