2318f859044a7d3f248e0823503fb692651276d7
FedeGen12 <fedegen14@gmail.com>
2024-05-24 00:24:53 -0300

    Ejercicio 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,22 +4,52 @@ function App() {
   const [goodComents, setGood] = useState(0);
   const [neutralComents, setNeutral] = useState(0);
   const [badComents, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
+
 
   let increaseValue = (setValue, currValue) => {
-    return () => { setValue(currValue + 1); }
+      let newValue = currValue + 1;
+      setValue(newValue);
+      return newValue;
+  }
+
+  let increaseGood = () => {
+    let newValue = increaseValue(setGood, goodComents);
+    let newTotal = increaseValue(setTotal, total);
+    setAverage((newValue - badComents) / newTotal);
+    setPositive(newValue / newTotal * 100);
+  }
+
+  let increaseNeutral = () => {
+    let newValue = increaseValue(setNeutral, neutralComents);
+    let newTotal = increaseValue(setTotal, total);
+    setTotal(newTotal);
+    setPositive(goodComents / newTotal * 100);
+  }
+
+  let increaseBad = () => {
+    let newValue = increaseValue(setBad, badComents);
+    let newTotal = increaseValue(setTotal, total);
+    setAverage((goodComents - newValue) / newTotal);
+    setPositive(goodComents / newTotal * 100);
   }
 
   return (
       <div>
         <p>Give Feedback!</p>
-        <button onClick={increaseValue(setGood, goodComents)}>Good</button>
-        <button onClick={increaseValue(setNeutral, neutralComents)}>Neutral</button>
-        <button onClick={increaseValue(setBad, badComents)}>Bad</button>
+        <button onClick={increaseGood}>Good</button>
+        <button onClick={increaseNeutral}>Neutral</button>
+        <button onClick={increaseBad}>Bad</button>
 
         <p>Statistics</p>
         <p>Good = {goodComents}</p>
         <p>Neutral = {neutralComents}</p>
         <p>Bad = {badComents}</p>
+        <p>All = {total}</p>
+        <p>Average = {average}</p>
+        <p>Positive = {positive}%</p>
       </div>
   );
 }
