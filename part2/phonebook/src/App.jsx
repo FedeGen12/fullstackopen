ec68201d053939ad32734d5ddab9baf99f1a54bc
Federico Genaro <fedegen14@gmail.com>
2024-10-08 12:38:05 -0300

    Exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -8,6 +8,9 @@ const App = () => {
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
 
+    const [personsFilter, setNewPersonsFilter] = useState(persons)
+    const [newFilter, setNewFilter] = useState('')
+
     const addPerson = (event) => {
         event.preventDefault()
 
@@ -38,27 +41,53 @@ const App = () => {
         setNewValue(event.target.value)
     }
 
+    const handlerFilterChange = (event) => {
+        const currFilter = event.target.value
+        setNewFilter(currFilter)
+
+        // Check the currFilter without the spaces behind the word
+        if (currFilter.trimStart().length === 0) {
+            setNewPersonsFilter(persons)    // shows the original persons list
+            return true;
+        }
+
+        setNewPersonsFilter(persons.filter(person => {
+            const words = person.name.split(' ').map(word => word.toLowerCase())
+
+            let is_filter = false
+            words.forEach(word => word.startsWith(currFilter.toLowerCase()) ? is_filter = true : false)
+            return is_filter
+        }))
+    }
+
     return (
         <div>
             <h2>Phonebook</h2>
-            <form onSubmit={addPerson}>
                 <div>
-                    name: <input value={newName}
-                                 onChange={(event) => handlerInputChange(event, setNewName)} />
+                    filter shown with name or surname: <input value={newFilter}
+                                                              onChange={handlerFilterChange} />
                 </div>
-                
-                <div>
-                    number: <input value={newNumber}
-                                   onChange={(event) => handlerInputChange(event, setNewNumber)} />
-                </div>
-                
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
+
+            <h2>Add a new</h2>
+                <form onSubmit={addPerson}>
+                    <div>
+                        name: <input value={newName}
+                                     onChange={(event) => handlerInputChange(event, setNewName)} />
+                    </div>
+
+                    <div>
+                        number: <input value={newNumber}
+                                       onChange={(event) => handlerInputChange(event, setNewNumber)} />
+                    </div>
+
+                    <div>
+                        <button type="submit">add</button>
+                    </div>
+                </form>
+
             <h2>Numbers</h2>
                 <div>
-                    {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+                    {personsFilter.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
                 </div>
         </div>
     )
