1f558d4e46b9a4b6bead93feab641c1a46ee44a9
Federico Genaro <fedegen14@gmail.com>
2024-10-11 21:06:09 -0300

    New module services/notes.js for backend communication

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,23 +1,19 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
     const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('')
     const [showAll, setShowAll] = useState(true)
 
-    const hook = () => {
-        console.log('effect')
-        axios
-            .get('http://localhost:3001/notes')
-            .then(response => {
-                console.log('promise fulfilled')
-                setNotes(response.data)
+    useEffect(() => {
+        noteService
+            .getAll()
+            .then(initialNotes => {
+                setNotes(initialNotes)
             })
-    }
-
-    useEffect(hook, [])
+    }, [])
 
     const addNote = event => {
         event.preventDefault()
@@ -26,11 +22,11 @@ const App = () => {
             important: Math.random() < 0.5,
         }
 
-
-        axios
-            .post('http://localhost:3001/notes', noteObject)
-            .then(response => {
-                console.log(response)
+        noteService
+            .create(noteObject)
+            .then(returnedNote => {
+                setNotes(notes.concat(returnedNote))
+                setNewNote('')
             })
     }
 
@@ -43,13 +39,14 @@ const App = () => {
         : notes.filter(note => note.important)
 
     const toggleImportanceOf = (id) => {
-        const url = `http://localhost:3001/notes/${id}`
         const note = notes.find(n => n.id === id)
         const changedNote = { ...note, important: !note.important }
 
-        axios.put(url, changedNote).then(response => {
-            setNotes(notes.map(note => note.id !== id ? note : response.data))
-        })
+        noteService
+            .update(id, changedNote)
+            .then(returnedNote => {
+                setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+            })
     }
 
     return (
