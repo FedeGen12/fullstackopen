7148c03f72c16f86b4ebfc9cd49dd1b85ae4f40e
Federico Genaro <fedegen14@gmail.com>
2025-01-02 14:56:34 -0300

    Exercise 3.10

--- part2_5/phonebook/src/services/persons.jsx
+++ part2_5/phonebook/src/services/persons.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const personsURL = 'http://localhost:3001/api/persons'
+const personsURL = '/api/persons'
 
 const getAllPersons = () => {
     const request= axios.get(personsURL)
